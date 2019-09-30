b43948e9d999a74d4b721c5c9222097696de1b00
tomvsas <ps178523@summacollege.nl>
2019-09-30 16:20:27 +0200

    added music packages

--- Sounds.js
+++ Sounds.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Button } from 'react-native';
+import SoundPlayer from 'react-native-sound-player'
+import Sound from 'react-native-sound';
 
 export default class Fleet extends React.Component {
     render() {
         return (
             <View>
-                <Text>The sound screen</Text>
+                <Button onPress={ playMusic2 } title="Play"></Button>
             </View>
         )        
     }
+}
+
+const playMusic1 = () => {
+    try {
+        // or play from url
+        //SoundPlayer.playUrl('https://music.youtube.com/watch?v=P_7OWVzChos&list=PLTQImNwft-Cw_ut4MAekHzOfWnBnRQYMI')
+        SoundPlayer.playSoundFile('victory_day', 'mp3');
+    } catch (e) {
+        console.log(`cannot play the sound file`, e)
+    }
+};
+
+const playMusic2 = () => {
+    var music = new Sound('victory_day.mp3', Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+          console.log('failed to load the sound', error);
+          return;
+        }
+        // loaded successfully
+        console.log('duration in seconds: ' + music.getDuration() + 'number of channels: ' + music.getNumberOfChannels());
+       
+        // Play the sound with an onEnd callback
+        music.play((success) => {
+          if (success) {
+            console.log('successfully finished playing');
+          } else {
+            console.log('playback failed due to audio decoding errors');
+          }
+        });
+      });
 }
